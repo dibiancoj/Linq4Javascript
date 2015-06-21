3ec56bce57579aba8af9247f949ec95f30f7d648
dibiancoj <DiBiancoj@gmail.com>
2015-06-21 16:42:11 -0400

    more async setup. Working web worker with a timeout to make sure the clock is still working.

--- Linq4Javascript/Scripts/AsyncTester.ts
+++ Linq4Javascript/Scripts/AsyncTester.ts
@@ -13,6 +13,13 @@ function RunQuery() {
 
     document.getElementById('Results').innerHTML = 'Running Query...';
 
+    inlineWorker.postMessage(1);
+    inlineWorker.postMessage(2);
+    inlineWorker.postMessage(100);
+    //scriptTagWorker.postMessage(1);
+    //scriptTagWorker.postMessage(2);
+    //scriptTagWorker.postMessage(100);
+
 }
 
 function DisplayResults(Results: Array<TestData>) {
@@ -27,3 +34,38 @@ function DisplayResults(Results: Array<TestData>) {
 
     document.getElementById('Results').innerHTML = html;
 }
+
+///////////////////////////// 
+
+//Make worker renders the script based on the string script
+function MakeWorker(Script: String) {
+
+    //TODO: Modernizer to make sure we have everything
+    //var URL = window.URL || window.webkitURL;
+    //var Blob = window.Blob;
+    //var Worker = window.Worker;
+
+    //if (!URL || !Blob || !Worker || !Script) {
+    //    return null;
+    //}
+
+    return new Worker(URL.createObjectURL(new Blob([Script])));
+}
+
+// Load a worker from a string, and manually initialize the worker (this does the work...and returns the data)
+var inlineWorkerText = "self.addEventListener('message', function(e) { setTimeout(function(){  postMessage(e.data * 2); },5000)} ,false);";
+
+var inlineWorker = MakeWorker(inlineWorkerText);
+
+//incase you want to troubleshoot
+inlineWorker.onmessage = e => {
+    document.getElementById('log').innerHTML += '<br />Inline: ' + e.data;
+};
+
+
+// Load a worker from a script of type=text/worker, and use the getWorker helper
+//var scriptTagWorker = MakeWorker(document.getElementById('worker-script').textContent);
+
+//scriptTagWorker.onmessage = e => {
+//    document.getElementById('log').innerHTML += '<br />Script Tag: ' + e.data;
+//};
